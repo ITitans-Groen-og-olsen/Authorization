768cdc7f46e7339f46b4d73dfb6a4a78247aecd1
HjalteHH <hjal0550@skivecollege.dk>
2025-05-14 12:10:33 +0200

    finished connection between vault and token generator. token secret hardcoded for now

--- AuthorizationApi/Controllers/AuthController.cs
+++ AuthorizationApi/Controllers/AuthController.cs
@@ -6,6 +6,10 @@ using System.Security.Claims;
 using System.Text;
 using AuthorizationApi.Model;
 namespace AuthorizationApi.Controllers;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.Commons;
 
 [ApiController]
 [Route("[controller]")]
@@ -44,15 +48,53 @@ private string GenerateJwtToken(string email)
 [HttpPost("login")]
 public async Task<IActionResult> Login([FromBody] Login login)
 {
-    if (login.EmailAddress == "haavy_user" && login.Password == "aaakodeord")
+    var secret = await GetSecret(login, _config);
+    if (login.Password == secret.ToString())
     {
         var token = GenerateJwtToken(login.EmailAddress);
         return Ok(new { token });
     }
-
     return Unauthorized();
 }
 
+public async Task<string>GetSecret(Login login, IConfiguration config)
+{
+    var endPoint = config["VaultName"] ?? "<blank>";
 
+    var httpClientHandler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) => true
+    };
 
+    // Initialize one of the several auth methods.
+    IAuthMethodInfo authMethod = new TokenAuthMethodInfo("00000000-0000-0000-0000-000000000000");
+
+    // Initialize settings. You can also set proxies, custom delegates, etc. here.
+    var vaultClientSettings = new VaultClientSettings(endPoint, authMethod)
+    {
+        Namespace = "",
+        MyHttpClientProviderFunc = handler => new HttpClient(httpClientHandler)
+        {
+            BaseAddress = new Uri(endPoint)
+        }
+    };
+
+    IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+
+    // Use client to read a key-value secret.
+    try
+    {
+        Secret<SecretData> kv2Secret = await vaultClient.V1.Secrets.KeyValue.V2
+            .ReadSecretAsync(path: "passwords", mountPoint: "secret");
+
+        var minkode = kv2Secret.Data.Data[login.EmailAddress];
+
+        return minkode?.ToString();
+    }
+    catch (Exception ex)
+    {
+        _logger.LogError(ex, "Error retrieving secret from Vault");
+        return string.Empty;
+    }
+}
 }
